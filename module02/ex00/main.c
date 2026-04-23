3d8a6406f74559a53e6879086bd35c1feee6ce09
siligh <siligh@f1br7s10.paris.42.school>
2026-04-23 14:38:22 +0200

    init fini

---
+++ module02/ex00/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siligh <siligh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 14:06:10 by siligh            #+#    #+#             */
+/*   Updated: 2026/04/23 14:38:04 by siligh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+void uart_init(void)
+{
+    UBRR0L = 16; // Writing UBRRnL will trigger an immediate update of the baud rate prescaler (20.11.5)
+    
+    UCSR0A |= (1 << U2X0); //Double the USART Transmission Speed (20.11.2)
+    
+    UCSR0B |= (1 << TXEN0); //enable transmitter (20.11.3)
+    
+    UCSR0C &= ~(1 << UPM01);
+    UCSR0C &= ~(1 << UPM00); // pas de parite
+
+    UCSR0C &= ~(1 << USBS0); // Stop Bit = 1bit
+
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); //On code l'info sur 8bit
+    
+}
+
+int main()
+{
+   while(1){} 
+}
